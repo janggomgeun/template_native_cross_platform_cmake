9f77e8fea8ca14c70f22cfbbd755b96b8c4260e7
Jang Woong Geun <janggomgeun@gmail.com>
2022-12-10 17:25:31 +0900

    build: initialize the project

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#define DOCTEST_CONFIG_IMPLEMENT
+
+int main(int /*argc*/, char** /*argv*/)
+{
+  {
+    // doctest::Context context;
+    // context.applyCommandLine(argc, argv);
+    // return context.run();
+  }
+  return 0;
+}
