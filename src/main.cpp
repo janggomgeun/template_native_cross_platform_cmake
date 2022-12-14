a12e75ccb616a9ac4d9bec3b5e13be679d0c326f
Jang Woong Geun <janggomgeun@gmail.com>
2022-12-14 11:36:23 +0900

    build: setup unit testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
 #define DOCTEST_CONFIG_IMPLEMENT
 
-int main(int /*argc*/, char** /*argv*/)
+#include <iostream>
+
+#include "doctest/doctest.h"
+#include "spdlog/spdlog.h"
+
+int main(int argc, char** argv)
 {
-  {
-    // doctest::Context context;
-    // context.applyCommandLine(argc, argv);
-    // return context.run();
-  }
+  spdlog::info("hmm");
+  std::cout << "print" << std::endl;
+
+  doctest::Context context;
+  context.applyCommandLine(argc, argv);
+  return context.run();
+
   return 0;
 }
